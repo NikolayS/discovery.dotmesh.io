23c550dd907c92f7154fdfceec7cfc96900a19fc
Luke Marsden <me@lukemarsden.net>
2016-10-30 12:13:15 +0100

    restart always

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 docker build -t lmarsden/discovery.data-mesh.io .
 HostIP=localhost
 docker rm -f etcd discovery traefik get
-docker run -d -v /usr/share/ca-certificates/:/etc/ssl/certs \
+docker run --restart=always -d -v /usr/share/ca-certificates/:/etc/ssl/certs \
  -v /pool/etcd-data:/var/lib/etcd \
  --name etcd \
  quay.io/coreos/etcd \
@@ -15,10 +15,10 @@ docker run -d -v /usr/share/ca-certificates/:/etc/ssl/certs \
  -initial-cluster-token etcd-cluster-1 \
  -initial-cluster etcd0=http://${HostIP}:2380 \
  -initial-cluster-state new
-docker run -d --link etcd -e DISC_ETCD=http://etcd:2379 \
+docker run --restart=always -d --link etcd -e DISC_ETCD=http://etcd:2379 \
  --name discovery \
  -e DISC_HOST=https://discovery.data-mesh.io lmarsden/discovery.data-mesh.io
-docker run --name get -v /pool/releases:/usr/share/nginx/html:ro --label traefik.port=80 -d nginx
-docker run --name traefik -d --link discovery --link get -p 8080:8080 -p 80:80 \
+docker run --restart=always --name get -v /pool/releases:/usr/share/nginx/html:ro --label traefik.port=80 -d nginx
+docker run --restart=always --name traefik -d --link discovery --link get -p 8080:8080 -p 80:80 \
  -v /var/run/docker.sock:/var/run/docker.sock \
  -v $PWD/traefik.toml:/etc/traefik/traefik.toml traefik
