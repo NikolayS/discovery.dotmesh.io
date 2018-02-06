e521ee5176a21ea69c64c45c2f22a8eb7e39f6ee
Releases <releases@get.dotmesh.io>
2018-02-06 17:06:37 +0000

    Ah, _this_ version of etcd works with this config.

--- start.sh
+++ start.sh
@@ -5,8 +5,8 @@ docker rm -f etcd discovery traefik get
 docker run --restart=always -d -v /usr/share/ca-certificates/:/etc/ssl/certs \
  -v /pool/etcd-data:/var/lib/etcd \
  --name etcd \
- quay.io/coreos/etcd:v3.2.9 \
- etcd -name etcd0 \
+ quay.io/coreos/etcd:v2.3.8 \
+ -name etcd0 \
  -data-dir /var/lib/etcd \
  -advertise-client-urls http://${HostIP}:2379,http://${HostIP}:4001 \
  -listen-client-urls http://0.0.0.0:2379,http://0.0.0.0:4001 \
